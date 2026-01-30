675fc372fe8a36fce42b2ca52b92db53eae6ceb4
brayanp11 <145622019+brayanp11@users.noreply.github.com>
2026-01-29 23:11:45 -0400

    Time: 3 ms (63.29%), Space: 19.3 MB (25.38%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -2,6 +2,7 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         low = 0
         high = x
+        ans = 0
         while low <= high:
             mid = (low + high) // 2
             if mid*mid == x:
@@ -9,5 +10,6 @@ class Solution:
             elif mid*mid > x:
                 high = mid - 1
             else:
+                ans = mid
                 low = mid + 1
-        return low - 1
+        return ans
