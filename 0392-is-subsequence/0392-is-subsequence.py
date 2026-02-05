8541e1f00ea271ce9d09ec94c61f00cd2636cc6f
brayanp11 <145622019+brayanp11@users.noreply.github.com>
2026-02-05 15:57:53 -0400

    Time: 0 ms (100%), Space: 19.3 MB (37.23%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,14 +2,14 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) > len(t):
             return False
-        if len(s) <= 0:
-            return True
+        """if len(s) <= 0:
+            return True"""
 
         slow, fast = 0, 0
         while fast < len(t) and slow < len(s):
             if fast < len(t) and s[slow] != t[fast]:
                 fast += 1
-            elif fast < len(t) and slow < len(s) and s[slow] == t[fast]:
+            elif fast < len(t) and s[slow] == t[fast]:
                 slow += 1
                 fast += 1
         return slow == len(s)
