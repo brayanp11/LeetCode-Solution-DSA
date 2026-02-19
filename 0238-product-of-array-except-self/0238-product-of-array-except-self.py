f860a1328158ee28b6fcfa6436e6102f8f3b4a58
brayanp11 <145622019+brayanp11@users.noreply.github.com>
2026-02-19 16:47:30 -0400

    Time: 15 ms (94.6%), Space: 25.8 MB (35.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,9 +6,9 @@ class Solution:
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-        
+
         suffix = 1
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums) -1, -1, -1):
             res[i] *= suffix
             suffix *= nums[i]
 
