ac3c07716bf2cdf83f4337e30808aea92e76b210
brayanp11 <145622019+brayanp11@users.noreply.github.com>
2026-02-20 23:30:43 -0400

    Time: 68 ms (22.6%), Space: 19.5 MB (46.09%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,4 +1,9 @@
-from itertools import chain
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        return list(chain.from_iterable(zip(nums[:n], nums[n:])))
+        res = []
+
+        for x, y in zip(nums[:n], nums[n:]):
+            res.append(x)
+            res.append(y)
+        
+        return res
