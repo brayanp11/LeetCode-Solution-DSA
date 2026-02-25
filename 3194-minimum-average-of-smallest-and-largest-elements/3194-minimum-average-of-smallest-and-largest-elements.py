1a3c49b0cb82fe68ef56ee8265ec51344b727b01
brayanp11 <145622019+brayanp11@users.noreply.github.com>
2026-02-25 17:32:02 -0400

    Time: 0 ms (100%), Space: 19.5 MB (46.17%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.py
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.py
@@ -1,13 +1,15 @@
-from collections import deque
 class Solution:
     def minimumAverage(self, nums: List[int]) -> float:
         nums.sort()
-        nums = deque(nums)
-        avg = 0
-        res = []
-        while nums:
-            maxElement = nums.pop()
-            minElement = nums.popleft()
-            avg = (minElement + maxElement) / 2
-            res.append(avg)
-        return min(res)
+        left, right = 0, len(nums) - 1
+        min_avg = float('inf')
+        
+        while left < right:
+            avg = (nums[left] + nums[right]) / 2
+
+            if avg < min_avg:
+                min_avg = avg
+            
+            left += 1
+            right -= 1
+        return min_avg
